cb9f2aea68fe490f29b4504d4d8ab4ba791b4b9d
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2022-05-31 21:38:37 +0200

    :sparkles: Adds project name to build

--- src/commands/build.c
+++ src/commands/build.c
@@ -1,17 +1,24 @@
 #include "build.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include "../helpers/colors.h"
 #include "../helpers/dir.h"
+#include "../helpers/def.h"
 #include "../utils/exec.h"
 #include "../utils/cmake_get.h"
 
 void build() {
-//    char *project_name = cmake_get("project");
-//    printf("proj '%s'\n", project_name);
-//    return;
+    char *project_name = cmake_get("project");
+    bool delete = false;
+    for (int i = 0; i < strlen(project_name); i++) {
+        if (project_name[i] == ' ' || delete) {
+            project_name[i] = '\0';
+            if (!delete) delete = true;
+        }
+    }
 
     DIR *dir = opendir(BUILD_PATH);
     if (dir == NULL) {
@@ -20,6 +27,8 @@ void build() {
     else {
         closedir(dir);
     }
+
+    printf("Building " BOLD "%s" RESET "\n", project_name);
 
     change_dir(BUILD_PATH);
 
@@ -36,5 +45,5 @@ void build() {
     }
 
     change_dir("..");
-    printf("Your executable is located in " BOLD "cgen-build/" RESET "\n");
+    printf("Your executable is located in " BOLD "cgen-build/%s" RESET "\n", project_name);
 }
